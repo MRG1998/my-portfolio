b251116ee1cd381d8360752dff95b9f65eb8ada5
abc <abc@user.email>
2021-07-26 11:50:18 +0300

    portfilio1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SkillsComponent,
+    AboutComponent,
+    ProjectComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'home', component:HomeComponent},
+      {path: 'skills', component:SkillsComponent},
+      {path: 'about', component:AboutComponent},
+      {path: 'project', component:ProjectComponent},
+      {path: 'contact', component:ContactComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
